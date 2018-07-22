ed6521b4b24de04066bfcce98f723867fec5c5a7
John Erickson <ericksonjohn08@hotmail.com>
2018-07-21 21:58:51 -0400

    Create src and screens directories. Create login_screen file. Separate code logic into login_screen file.

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// this creates the widget which is show on the screen of the device. This widget gets created and thrown away every time the application is rendered
 class LoginScreen extends StatefulWidget {
   @override
     State<StatefulWidget> createState() {
@@ -7,17 +8,21 @@ class LoginScreen extends StatefulWidget {
     }
 }
 
+// this creates the login data that is persisted to hold the values 
 class _LoginScreenState extends State<LoginScreen> {
   @override
     Widget build(BuildContext context) {
       return Container(
         margin: EdgeInsets.all(20.0),
-        child: Form(
+        child: Form( // Form is a instance variable that behind the scenes also acts as a StatefulWidget
           child: Column(
             children: <Widget>[
               emailField(),
-              // passwordField(),
-              // submitButton(),
+              passwordField(),
+              SizedBox(
+                height: 25.0, // this will create a modular solution which will make any refactore more easy
+              ),
+              submitButton(),
             ],
           ),
         ),
@@ -30,15 +35,30 @@ class _LoginScreenState extends State<LoginScreen> {
           labelText: 'Email Address',
           hintText: 'you@example.com',
         ),
-        keyboardType: TextInputType.emailAddress,
+        keyboardType: TextInputType.emailAddress, // emailAddress is a constant of the TextFormField class
       );
     }
 
     Widget passwordField() {
-
+      return TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+          hintText: 'Password',
+        ),
+        obscureText: true, // sets the password fields to bullet points
+      );
     }
 
     Widget submitButton() {
-
+      return RaisedButton(
+        child: Text('Submit'),
+        color: Colors.green[400],
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0)
+        ),
+        onPressed: () {
+          
+        },
+      );
     }
 }
